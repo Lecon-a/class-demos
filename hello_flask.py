adce77a8b03d6154c76b2aa6167e92373bba0e41
Sunday P. Afolabi <spog4true@gmail.com>
2022-07-23 19:45:58 -0400

    import flask-sqlalchemy to the web server

--- hello_flask.py
+++ hello_flask.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import dsn
 
-app = Flask(__name__)
+hello_flask = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = dsn.URL
+db = SQLAlchemy(app)
 
-@app.route("/")
+@hello_flask.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+# running flask app option 2
+# if __name__=='__main__':
+#     hello_flask.run(host="0.0.0.0")    
